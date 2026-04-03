d6179032436e936c3f079802fd9868cb93da2925
yangxiuyu <gzzyyxy@gmail.com>
2026-04-03 15:18:50 +0800

    driver check

---
+++ driver_check.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+function get_script_path()
+{
+    if [ -n "$BASH_SOURCE" ]; then
+        # Bash shell
+        _SCRIPT_PATH=$(readlink -f "$BASH_SOURCE")
+        echo $_SCRIPT_PATH
+        return 0
+    elif [ -n "$ZSH_NAME" ]; then
+        # Zsh shell
+        _SCRIPT_PATH=$(dirname "$0")/$(basename "$0")
+        _SCRIPT_PATH=$(readlink -f "$SCRIPT_PATH")
+        echo $_SCRIPT_PATH
+        return 0
+    else
+        echo "Unknown shell type"
+        return 1
+    fi
+}
+function set_proxy()
+{
+    rich -p "[bold cyan]> Setting proxy...[/]"
+    export http_proxy='http://sys-proxy-rd-relay.byted.org:8118'
+    export https_proxy='http://sys-proxy-rd-relay.byted.org:8118'
+    export no_proxy='.byted.org'
+}
+function unset_proxy()
+{
+    unset http_proxy
+    unset https_proxy
+    unset no_proxy
+    rich -p "[bold cyan]> Proxy unset[/]"
+}
+SCRIPT_DIR=$(dirname $(get_script_path))
+SIMKIT_DIR=$(realpath $SCRIPT_DIR/../)
+cd $SIMKIT_DIR
+set_proxy
+uv tool install rich-cli
+REQUIRED_VERSION_MAJOR=535
+REQUIRED_VERSION_MINOR=129
+rich -p "[bold cyan]> Checking available Vulkan drivers for IsaacSim...(Minimum required version: $REQUIRED_VERSION_MAJOR.$REQUIRED_VERSION_MINOR)[/]"
+DRV_HEX=$(vulkaninfo 2>&1 | awk '/driverVersion/ {print $3}')
+VERSIONS=( $DRV_HEX )
+FIND_AVAILABLE_DRIVER=false
+for DRV in ${VERSIONS[@]}; do
+    MAJOR=$(( (DRV >> 22) & 0x3ff ))
+    MINOR=$(( (DRV >> 14) & 0x0ff ))
+    PATCH=$(( (DRV >> 6)  & 0x0ff ))
+    # RTX gate
+    if [ $MAJOR -lt $REQUIRED_VERSION_MAJOR ] || \
+    { [ $MAJOR -eq $REQUIRED_VERSION_MAJOR ] && [ $MINOR -lt $REQUIRED_VERSION_MINOR ]; }; then
+        rich -p "[dim]   Unsupported driver: $MAJOR.$MINOR.$PATCH[/]"
+    else
+        rich -p "[bold green]   Found available driver: $MAJOR.$MINOR.$PATCH[/]"
+        FIND_AVAILABLE_DRIVER=true
+        break
+    fi
+done
+if [ "$FIND_AVAILABLE_DRIVER" = false ]; then
+    rich -p "[bold red]> No available driver can be found on this machine, maybe you want add this IP to blacklist?[/]"
+    if [ -n "$MY_HOST_IP" ]; then
+        rich -p "[bold dim]   $MY_HOST_IP[/]"
+    fi
+    if [ -n "$MY_HOST_IPV6" ]; then
+        rich -p "[bold dim]   $MY_HOST_IPV6[/]"
+    fi
+    exit 1
+fi
